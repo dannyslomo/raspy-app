9106e16e2a21fd7947ccdeaf55b493f37d8ab427
Tim Rettstatt <timrettstatt2000@gmail.com>
2023-05-13 16:53:28 +0200

    added url class

---
+++ app/src/main/java/de/hhn/softwarelab/raspy/backend/dataclasses/Url.kt
@@ -0,0 +1,8 @@
+package de.hhn.softwarelab.raspy.backend.dataclasses
+
+class Url {
+    companion object{
+        val livestreamUrl = "rtsp://192.168.109.88:8554/video_stream"
+        val serverUrl = "http://192.168.109.209:8000/"
+    }
+}
